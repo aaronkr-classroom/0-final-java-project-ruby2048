4aa000fc4524daad38423fe4bcbcd38603066dbe
ruby2048 <dkssudtmdwls@naver.com>
2024-03-12 17:19:44 +0900

    level3 end
    end

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Welcome {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("당신의 이름을 입력하세요 : ");
+		String name = input.nextLine();
+		
+		System.out.println("연락처를 입력하세요");
+		int phone = input.nextInt();
+		
 		String greeting = "Welcome to Shopping Mall";
 		String tagline = "Welcome to Book Market!!";
 		
@@ -12,5 +22,9 @@ public class Welcome {
 		System.out.println("3. 장바구니 비우기\t7. 장바구니의 항목 삭제하기");
 		System.out.println("4. 영수증 표시하기\t8. 종료");
 		System.out.println("********************************************");
+		
+		System.out.println("메뉴 번호를 선택하세요 : ");
+		int choice = input.nextInt();
+		System.out.println(choice + "번을 선택했습니다.");
 	}
 }
